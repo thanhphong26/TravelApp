a05e6dee1bc4e87e9037ea405805313502ea01bb
Thanh Phong <thanh2652003abc@gmail.com>
2024-03-17 15:06:43 +0700

    TA-05 | Add detail hotel, detail restaurant section

--- app/src/main/java/com/travel/Activity/DetailHotelActivity.java
+++ app/src/main/java/com/travel/Activity/DetailHotelActivity.java
@@ -6,11 +6,11 @@ import android.os.Bundle;
 
 import com.travel.R;
 
-public class MainActivity extends AppCompatActivity {
+public class DetailHotelActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail_restaurant);
+        setContentView(R.layout.activity_detail_hotel);
     }
 }
