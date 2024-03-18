b01c397b98e87a378a7213465614a0a240663e9f
Thanh Phong <94186669+thanhphong26@users.noreply.github.com>
2024-03-18 23:28:28 +0700

    TA-05 | Add flight section

--- app/src/main/java/com/travel/Activity/FlightActivity.java
+++ app/src/main/java/com/travel/Activity/FlightActivity.java
@@ -6,11 +6,11 @@ import android.os.Bundle;
 
 import com.travel.R;
 
-public class MainActivity extends AppCompatActivity {
+public class FlightActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail_hotel);
+        setContentView(R.layout.activity_flight);
     }
 }
