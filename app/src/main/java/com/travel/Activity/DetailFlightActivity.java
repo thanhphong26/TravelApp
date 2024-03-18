ebdfd112f63cfed71ec61df123e65edbfcf92da9
Thanh Phong <thanh2652003abc@gmail.com>
2024-03-19 00:07:12 +0700

    TA-05 | Add detail flight section

--- app/src/main/java/com/travel/Activity/DetailFlightActivity.java
+++ app/src/main/java/com/travel/Activity/DetailFlightActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 
 import com.travel.R;
 
-public class MainActivity extends AppCompatActivity {
+public class DetailFlightActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
