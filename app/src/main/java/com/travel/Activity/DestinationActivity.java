89f5fbcb22cbcf2e47e54875449768a1b0365271
Thanh Tien <phungthanhtien01@gmail.com>
2024-03-18 22:08:20 +0700

    TA-03 | Implement tour section

---
+++ app/src/main/java/com/travel/Activity/DestinationActivity.java
@@ -0,0 +1,16 @@
+package com.travel.Activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.travel.R;
+
+public class DestinationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_destination);
+    }
+}
