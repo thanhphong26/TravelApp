a75da2f2e32aa93d724c0f70581e2a33143575a6
Thanh Tien <phungthanhtien01@gmail.com>
2024-03-20 15:40:23 +0700

    TA-03 | Handle open others page

--- app/src/main/java/com/travel/Activity/HomeActivity.java
+++ app/src/main/java/com/travel/Activity/HomeActivity.java
@@ -1,6 +1,9 @@
 package com.travel.Activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,5 +18,48 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
         bottomNavigationView = findViewById(R.id.navigation);
         bottomNavigationView.setItemIconTintList(null);
+        this.initPageOpen();
+    }
+
+    public void initPageOpen() {
+        LinearLayout ly_tour = (LinearLayout) findViewById(R.id.tour_page);
+        LinearLayout ly_hotel = (LinearLayout) findViewById(R.id.hotel_page);
+        LinearLayout ly_restaurant = (LinearLayout) findViewById(R.id.restaurant_page);
+        LinearLayout ly_flight = (LinearLayout) findViewById(R.id.flight_page);
+        ly_tour.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Open another layout here
+                Intent intent = new Intent(HomeActivity.this, TourActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ly_hotel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Open another layout here
+                Intent intent = new Intent(HomeActivity.this, HotelActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ly_restaurant.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Open another layout here
+                Intent intent = new Intent(HomeActivity.this, RestaurantActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ly_flight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Open another layout here
+                Intent intent = new Intent(HomeActivity.this, DestinationActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
