111bb30a2354cc50974893d5e488d9a70fa7a647
Thanh Tien <phungthanhtien01@gmail.com>
2024-03-20 16:20:29 +0700

    TA-03 | Implement search & favorite page

--- app/src/main/java/com/travel/Activity/MainActivity.java
+++ app/src/main/java/com/travel/Activity/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_home);
+//        setContentView(R.layout.activity_main);
 
         //*INFO: Open homepage
         Intent intent = new Intent(MainActivity.this, HomeActivity.class);
---
+++ app/src/main/java/com/travel/Activity/SearchActivity.java
@@ -0,0 +1,19 @@
+package com.travel.Activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.travel.R;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hotel);
+    }
+}
