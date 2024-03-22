0bb2b337ef2c31f917ba2814114154c013ee1d6f
TranNhatNguyen2003 <nguyentrandcm@gmail.com>
2024-03-22 22:43:38 +0700

    TA-04 | Edit Checkout, Payment Method

--- app/src/main/java/com/travel/Activity/MainActivity.java
+++ app/src/main/java/com/travel/Activity/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_book_flight);
+        setContentView(R.layout.activity_payment_method);
     }
 }
