496caa1fb9b3d47c52cd0bdc0cdc3648bbc6c7cc
Thanh Phong <thanh2652003abc@gmail.com>
2024-03-22 08:28:20 +0700

    TA-05 | Update detail tour, restaurant, hotel

--- app/src/main/java/com/travel/Activity/DetailTourActivity.java
+++ app/src/main/java/com/travel/Activity/DetailTourActivity.java
@@ -3,14 +3,30 @@ package com.travel.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.travel.R;
 
 public class DetailTourActivity extends AppCompatActivity {
+    FloatingActionButton btnLove;
+    private boolean isHeartRed = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_tour);
+        btnLove=(FloatingActionButton) findViewById(R.id.fabLove);
+        btnLove.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                isHeartRed = !isHeartRed;
+                if (isHeartRed) {
+                    btnLove.setImageResource(R.drawable.red_heart);
+                } else {
+                    btnLove.setImageResource(R.drawable.heart);
+                }
+            }
+        });
     }
 }
