afe830b5f7fc6720759265557008d9023c7c9723
Thanh Tien <phungthanhtien01@gmail.com>
2024-03-31 11:54:03 +0700

    TA-04 | BookHotel, BookRestaurant, BookFlight, BookTour, Checkout, Review History. Payment Method
    TA-04 | Fix BookHotel, BookRestaurant, BookFlight, BookTour, Checkout
    
    TA-04 | Edit Checkout, Payment Method
    
    TA-04 | Fix Image
    
    TA-04 | Add TimeLine

---
+++ app/src/main/java/com/travel/Activity/BookHotelActivity.java
@@ -0,0 +1,18 @@
+package com.travel.Activity;
+
+import android.app.Notification;
+import android.os.Bundle;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.travel.R;
+
+public class BookHotelActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_hotel);
+
+    }
+}
