1a60c4728264aaf9b1bfcb6b3cdc4b6dc9013087
Thanh Tien <phungthanhtien01@gmail.com>
2024-04-18 20:56:02 +0700

    TA-14 | Convert search page and productType page using adapter

--- app/src/main/java/com/travel/Activity/SearchActivity.java
+++ app/src/main/java/com/travel/Activity/SearchActivity.java
@@ -10,8 +10,12 @@ import android.widget.SearchView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.bumptech.glide.Glide;
+import com.travel.Adapter.HotelCommonAdapter;
+import com.travel.Adapter.HotelFavoriteAdapter;
+import com.travel.Adapter.TourFavoriteAdapter;
 import com.travel.Database.HotelDAO;
 import com.travel.Database.RestaurantDAO;
 import com.travel.Database.TourDAO;
@@ -46,19 +50,21 @@ public class SearchActivity extends AppCompatActivity {
         Intent intent = getIntent();
         String query = intent.getStringExtra("search");
 
+        this.setupLayoutRecyclerView();
         this.initPage();
         this.handleSearchGlobal(query);
     }
 
-    public  void setDefaultValue() {
-        // Remove all view inside lySearchTour, lySearchHotel, lySearchRestaurant except label section
-        searchBinding.lySearchTour.removeViews(1, searchBinding.lySearchTour.getChildCount() - 1);
-        searchBinding.lySearchHotel.removeViews(1, searchBinding.lySearchHotel.getChildCount() - 1);
-        searchBinding.lySearchRestaurant.removeViews(1, searchBinding.lySearchRestaurant.getChildCount() - 1);
+    private void setupLayoutRecyclerView() {
+        LinearLayoutManager layoutManagerHotel = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        searchBinding.hotelRecycleView.setLayoutManager(layoutManagerHotel);
+        LinearLayoutManager layoutManagerTour = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        searchBinding.tourRecycleView.setLayoutManager(layoutManagerTour);
+        LinearLayoutManager layoutManagerRestaurant = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        searchBinding.restaurantRecycleView.setLayoutManager(layoutManagerRestaurant);
     }
 
     public void handleSearchGlobal(String query) {
-        this.setDefaultValue();
         searchBinding.searchDetail.setIconified(false);
         searchBinding.searchDetail.setQuery(query, false);
 
@@ -73,60 +79,14 @@ public class SearchActivity extends AppCompatActivity {
     }
 
     public void handleListTour() {
-        if (matchedTours.size() <= 0) {
-            searchBinding.lySearchTour.setVisibility(View.GONE);
-        } else {
-            searchBinding.lySearchTour.setVisibility(View.VISIBLE);
-            for (TourModel tour : matchedTours) {
-                TourFavoriteCardBinding tourFavoriteCardBinding = TourFavoriteCardBinding.inflate(getLayoutInflater());
-
-                Glide.with(this).load(tour.getImage()).centerCrop().into(tourFavoriteCardBinding.roundedCityImage);
-                tourFavoriteCardBinding.tvTourName.setText(tour.getName());
-                tourFavoriteCardBinding.tourFavoriteRating.setText(String.valueOf(tour.getRating()));
-                tourFavoriteCardBinding.ratingBar.setRating(tour.getRating());
-                tourFavoriteCardBinding.tvDescription.setText(tour.getDescription());
-                tourFavoriteCardBinding.tvPrice.setText(NumberHelper.getFormattedPrice(tour.getPrice()) + " đ");
-
-                tourFavoriteCardBinding.getRoot().setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Intent intent = new Intent(SearchActivity.this, DetailTourActivity.class);
-                        intent.putExtra("tourId", tour.getTourId());
-                        startActivity(intent);
-                    }
-                });
-
-                searchBinding.lySearchTour.addView(tourFavoriteCardBinding.getRoot());
-            }
-        }
+        System.out.println("Tour: " + matchedTours.size());
+        TourFavoriteAdapter<TourModel> adapter = new TourFavoriteAdapter<>(matchedTours, this);
+        searchBinding.tourRecycleView.setAdapter(adapter);
     }
 
     public void handleListHotel() {
-        if (matchedHotels.size() <= 0) {
-            searchBinding.lySearchHotel.setVisibility(View.GONE);
-        } else {
-            searchBinding.lySearchHotel.setVisibility(View.VISIBLE);
-            for (HotelModel hotel : matchedHotels) {
-                HotelFavoriteCardBinding hotelFavoriteCardBinding = HotelFavoriteCardBinding.inflate(getLayoutInflater());
-                Glide.with(this).load(hotel.getImage()).centerCrop().into(hotelFavoriteCardBinding.roundedCityImage);
-                hotelFavoriteCardBinding.tvHotelName.setText(hotel.getName());
-                hotelFavoriteCardBinding.tourFavoriteRating.setText(String.valueOf(hotel.getRating()));
-                hotelFavoriteCardBinding.ratingBar.setRating(hotel.getRating());
-                hotelFavoriteCardBinding.tvDescription.setText(hotel.getDescription());
-                hotelFavoriteCardBinding.tvPrice.setText(NumberHelper.getFormattedPrice(hotel.getPrice()) + " đ");
-
-                hotelFavoriteCardBinding.getRoot().setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Intent intent = new Intent(SearchActivity.this, DetailHotelActivity.class);
-                        intent.putExtra("hotelId", hotel.getHotelId());
-                        startActivity(intent);
-                    }
-                });
-
-                searchBinding.lySearchHotel.addView(hotelFavoriteCardBinding.getRoot());
-            }
-        }
+        HotelFavoriteAdapter<HotelModel> adapter = new HotelFavoriteAdapter<>(matchedHotels, this);
+        searchBinding.hotelRecycleView.setAdapter(adapter);
     }
 
     public void handleListRestaurant() {
@@ -136,12 +96,12 @@ public class SearchActivity extends AppCompatActivity {
             searchBinding.lySearchRestaurant.setVisibility(View.VISIBLE);
             for (RestaurantModel restaurant : matchedRestaurants) {
                 RestaurantFavoriteCardBinding restaurantFavoriteCardBinding = RestaurantFavoriteCardBinding.inflate(getLayoutInflater());
-                Glide.with(this).load(restaurant.getImage()).centerCrop().into(restaurantFavoriteCardBinding.roundedCityImage);
+                Glide.with(this).load(restaurant.getImage()).centerCrop().into(restaurantFavoriteCardBinding.restaurantRoundedCityImage);
                 restaurantFavoriteCardBinding.tvRestaurantName.setText(restaurant.getName());
-                restaurantFavoriteCardBinding.tourFavoriteRating.setText(String.valueOf(restaurant.getRating()));
-                restaurantFavoriteCardBinding.ratingBar.setRating(restaurant.getRating());
-                restaurantFavoriteCardBinding.tvDescription.setText(restaurant.getDescription());
-                restaurantFavoriteCardBinding.tvPrice.setText(NumberHelper.getFormattedPrice(restaurant.getPrice()) + " đ");
+                restaurantFavoriteCardBinding.restaurantFavoriteRating.setText(String.valueOf(restaurant.getRating()));
+                restaurantFavoriteCardBinding.restaurantFavoriteRatingBar.setRating(restaurant.getRating());
+                restaurantFavoriteCardBinding.restaurantFavoriteAddress.setText(restaurant.getDestination().getName());
+                restaurantFavoriteCardBinding.restaurantFavoritePrice.setText(NumberHelper.getFormattedPrice(restaurant.getPrice()) + " đ");
 
                 restaurantFavoriteCardBinding.getRoot().setOnClickListener(new View.OnClickListener() {
                     @Override
--- app/src/main/java/com/travel/Adapter/HotelFavoriteAdapter.java
+++ app/src/main/java/com/travel/Adapter/HotelFavoriteAdapter.java
@@ -6,38 +6,39 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.travel.Activity.DetailHotelActivity;
 import com.travel.Model.HotelModel;
 import com.travel.R;
+import com.travel.Utils.NumberHelper;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class HotelCommonAdapter<T> extends RecyclerView.Adapter<HotelCommonAdapter.HotelCommonViewHolder> {
+public class HotelFavoriteAdapter<T> extends RecyclerView.Adapter<HotelFavoriteAdapter.HotelCommonViewHolder> {
     private List<T> listItem;
     private Context context;
 
-    public HotelCommonAdapter(ArrayList<T> listItem, Context context) {
+    public HotelFavoriteAdapter(ArrayList<T> listItem, Context context) {
         this.listItem = listItem;
         this.context = context;
     }
 
     @NonNull
     @Override
-    public HotelCommonAdapter.HotelCommonViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(context).inflate(R.layout.hotel_common_card, parent, false);
+    public HotelFavoriteAdapter.HotelCommonViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.hotel_favorite_card, parent, false);
         return new HotelCommonViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull HotelCommonAdapter.HotelCommonViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull HotelFavoriteAdapter.HotelCommonViewHolder holder, int position) {
         T item = listItem.get(position);
         if (item instanceof HotelModel){
             bindHotelModel(holder, (HotelModel) item);
@@ -47,6 +48,10 @@ public class HotelCommonAdapter<T> extends RecyclerView.Adapter<HotelCommonAdapt
     private void bindHotelModel(HotelCommonViewHolder holder, HotelModel item) {
         holder.hotelName.setText(item.getName());
         Glide.with(context).load(item.getImage()).error(R.drawable.bg_test_hotel_common_card).into(holder.hotelImage);
+        holder.rating.setText(String.valueOf(item.getRating()));
+        holder.price.setText(NumberHelper.getFormattedPrice(item.getPrice()) + " đ");
+        holder.address.setText(item.getAddress());
+        holder.ratingBar.setRating(item.getRating());
 
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -65,12 +70,17 @@ public class HotelCommonAdapter<T> extends RecyclerView.Adapter<HotelCommonAdapt
 
     public class HotelCommonViewHolder extends RecyclerView.ViewHolder {
         ImageView hotelImage;
-        TextView hotelName;
+        TextView hotelName, rating, price, address, description;
+        RatingBar ratingBar;
 
         public HotelCommonViewHolder(@NonNull View itemView) {
             super(itemView);
-            hotelImage = itemView.findViewById(R.id.hotel_common_image);
-            hotelName = itemView.findViewById(R.id.hotel_common_name);
+            hotelImage = itemView.findViewById(R.id.roundedCityImage);
+            hotelName = itemView.findViewById(R.id.tv_hotel_name);
+            rating = itemView.findViewById(R.id.tour_favorite_rating);
+            price = itemView.findViewById(R.id.tv_price);
+            address = itemView.findViewById(R.id.tour_favorite_address);
+            ratingBar = itemView.findViewById(R.id.ratingBar);
         }
     }
 }
