690623706282bbd28a6d18791b0eb81d1025e8b2
gipboi <nguyenthanhdat1272003@gmail.com>
2024-04-22 17:14:36 +0700

    TA-12 | Update My History Booking

--- app/src/main/java/com/travel/Activity/HistoryActivity.java
+++ app/src/main/java/com/travel/Activity/HistoryActivity.java
@@ -6,8 +6,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.travel.Adapter.HistoryCardAdapter;
+import com.travel.Database.HotelBookingDAO;
+import com.travel.Database.RestaurantBookingDAO;
+import com.travel.Database.RestaurantDAO;
 import com.travel.Database.TourBookingDAO;
+import com.travel.Model.HotelBookingModel;
+import com.travel.Model.HotelBookingReviewModel;
+import com.travel.Model.RestaurantBookingModel;
+import com.travel.Model.RestaurantBookingReviewModel;
 import com.travel.Model.TourBookingModel;
+import com.travel.Model.TourBookingReviewModel;
 import com.travel.databinding.ActivityMyHistoryBinding;
 
 import java.util.List;
@@ -16,6 +24,10 @@ public class HistoryActivity extends AppCompatActivity {
     ActivityMyHistoryBinding binding;
     TourBookingDAO tourBookingDAO = new TourBookingDAO();
     TourBookingModel tourBooking = new TourBookingModel();
+    RestaurantBookingDAO restaurantDAO = new RestaurantBookingDAO();
+    HotelBookingDAO hotelDAO = new HotelBookingDAO();
+    List<HotelBookingModel> hotelBookingModels;
+    List<RestaurantBookingModel> restaurantBookingModels;
     List<TourBookingModel> tourBookingModels;
     // write onCreate method
     @Override
@@ -25,17 +37,36 @@ public class HistoryActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
         this.setupLayoutRecyclerView();
         this.displayTourBooking();
+        this.displayRestaurantBooking();
+        this.displayHotelBooking();
     }
     // create displayTourBooking method
     public void displayTourBooking() {
-        List<TourBookingModel> tourBookingModels = tourBookingDAO.getAllTourBookings();
-        System.out.println("Tour Booking: " + tourBookingModels.size());
-        HistoryCardAdapter<TourBookingModel> historyCardAdapter = new HistoryCardAdapter<>(tourBookingModels, this);
-        binding.recyclerViewHistory.setAdapter(historyCardAdapter);
+        List<TourBookingReviewModel> tourBookingModels = tourBookingDAO.getAllTourBookingsWithReview(2);
+        System.out.println(tourBookingModels.size());
+        HistoryCardAdapter<TourBookingReviewModel> historyCardAdapter = new HistoryCardAdapter<>(tourBookingModels, this);
+        binding.recyclerViewHistoryTour.setAdapter(historyCardAdapter);
+    }
+    // create displayRestaurantBooking method
+    public void displayRestaurantBooking() {
+        List<RestaurantBookingReviewModel> restaurantBookingModels = restaurantDAO.getAllRestaurantBookingsWithReview(2);
+        HistoryCardAdapter<RestaurantBookingReviewModel> historyCardAdapter = new HistoryCardAdapter<>(restaurantBookingModels, this);
+        binding.recyclerViewHistoryRestaurant.setAdapter(historyCardAdapter);
+    }
+    public void displayHotelBooking(){
+        List<HotelBookingReviewModel> hotelBookingModels = hotelDAO.getAllHotelBookingsWithReview(2);
+        System.out.println(hotelBookingModels.size());
+        HistoryCardAdapter<HotelBookingReviewModel> historyCardAdapter1 = new HistoryCardAdapter<>(hotelBookingModels, this);
+        binding.recyclerViewHistoryHotel.setAdapter(historyCardAdapter1);
+
     }
     //create setupLayoutRecyclerView method
     private void setupLayoutRecyclerView() {
-        LinearLayoutManager layoutManagerHistory = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
-        binding.recyclerViewHistory.setLayoutManager(layoutManagerHistory);
+        LinearLayoutManager layoutManagerHistoryTour = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        binding.recyclerViewHistoryTour.setLayoutManager(layoutManagerHistoryTour);
+        LinearLayoutManager layoutManagerHistoryHotel = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        binding.recyclerViewHistoryHotel.setLayoutManager(layoutManagerHistoryHotel);
+        LinearLayoutManager layoutManagerHistoryRestaurant = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        binding.recyclerViewHistoryRestaurant.setLayoutManager(layoutManagerHistoryRestaurant);
     }
 }
--- app/src/main/java/com/travel/Database/TourBookingDAO.java
+++ app/src/main/java/com/travel/Database/TourBookingDAO.java
@@ -6,7 +6,11 @@ import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 
 import com.travel.App;
+import com.travel.Model.ReviewModel;
+import com.travel.Model.ReviewType;
 import com.travel.Model.TourBookingModel;
+import com.travel.Model.TourBookingReviewModel;
+import com.travel.Model.TourModel;
 
 import java.sql.Timestamp;
 import java.util.ArrayList;
@@ -15,20 +19,29 @@ import java.util.List;
 public class TourBookingDAO {
     DatabaseHelper databaseHelper = new DatabaseHelper(App.self());
     SQLiteDatabase database;
+
     public TourBookingDAO() {
     }
+
     @SuppressLint("Range")
     //get all tour bookings from tour_booking table
-    public List<TourBookingModel> getAllTourBookings() {
-        List<TourBookingModel> tourBookingModels = new ArrayList<>();
+    public List<TourBookingReviewModel> getAllTourBookingsWithReview(int userId) {
+        List<TourBookingReviewModel> tourBookingModels = new ArrayList<>();
         database = databaseHelper.openDatabase();
         if (database != null) {
             try {
                 // get tour bookings by user id
-                Cursor cursor = database.rawQuery("SELECT * FROM tour_bookings WHERE user_id = ?" , null);
-                if (cursor.moveToFirst() ){
+                String query = "SELECT tour_bookings.*, reviews.*,tours.*\n" +
+                        "FROM tour_bookings\n" +
+                        "JOIN tours ON tour_bookings.tour_id = tours.tour_id\n" +
+                        "JOIN reviews ON tours.tour_id = reviews.item_id\n" +
+                        "WHERE reviews.review_type = 'tour' AND reviews.user_Id = 2;";
+                Cursor cursor = database.rawQuery(query, null);
+                System.out.println("cursor count: " + query);
+                if (cursor.moveToFirst()) {
+                    System.out.println("cursor count: " + cursor.getCount());
                     do {
-                        TourBookingModel tourBookingModel = new TourBookingModel();
+                        TourBookingReviewModel tourBookingModel = new TourBookingReviewModel();
                         tourBookingModel.setBookingId(cursor.getInt(cursor.getColumnIndex("booking_id")));
                         tourBookingModel.setTour(new TourDAO().getTourById(cursor.getInt(cursor.getColumnIndex("tour_id"))));
                         tourBookingModel.setBookingDate(cursor.getString(cursor.getColumnIndex("booking_date")));
@@ -36,13 +49,32 @@ public class TourBookingDAO {
                         tourBookingModel.setNumberOfAdults(cursor.getInt(cursor.getColumnIndex("number_of_adults")));
                         tourBookingModel.setNumberOfChildren(cursor.getInt(cursor.getColumnIndex("number_of_childs")));
                         tourBookingModel.setCreatedAt(Timestamp.valueOf(cursor.getString(cursor.getColumnIndex("created_at"))));
+
+                        TourModel tour = new TourModel();
+                        tour.setTourId(cursor.getInt(cursor.getColumnIndex("tour_id")));
+                        tour.setName(cursor.getString(cursor.getColumnIndex("name")));
+                        tour.setDescription(cursor.getString(cursor.getColumnIndex("description")));
+                        tour.setPrice(cursor.getFloat(cursor.getColumnIndex("price")));
+                        tour.setRating(cursor.getFloat(cursor.getColumnIndex("rating")));
+                        tour.setImage(cursor.getString(cursor.getColumnIndex("image")));
+                        tourBookingModel.setTour(tour);
+
+                        ReviewModel review = new ReviewModel();
+                        review.setReviewId(cursor.getInt(cursor.getColumnIndex("review_id")));
+                        review.setUserId(cursor.getInt(cursor.getColumnIndex("user_id")));
+//                        review.setReviewType(ReviewType.valueOf(cursor.getString(cursor.getColumnIndex("review_type"))));
+                        review.setItemId(cursor.getInt(cursor.getColumnIndex("item_id")));
+                        review.setReview(cursor.getString(cursor.getColumnIndex("review")));
+                        review.setRating(cursor.getInt(cursor.getColumnIndex("rating")));
+
+                        tourBookingModel.setReview(review);
+
                         tourBookingModels.add(tourBookingModel);
                     } while (cursor.moveToNext());
                 }
-            }catch (SQLException e) {
+            } catch (SQLException e) {
                 e.printStackTrace();
-            }
-            finally {
+            } finally {
                 databaseHelper.closeDatabase(database);
             }
         }
